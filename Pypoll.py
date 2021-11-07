94f5bfa291601bd3be6268d780a8a8266acab42f
mackenziecoushay <mackenziecoushay@Mackenzies-MacBook-Air.local>
2021-11-06 18:35:46 -0700

    saving changes

---
+++ Pypoll.py
@@ -0,0 +1,6 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates that received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on the popular vote
