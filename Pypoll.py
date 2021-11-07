ecf100f7cc74a0abaf2763980448fb7e7c68d0e2
mackenziecoushay <mackenziecoushay@Mackenzies-Air.domain>
2021-11-07 15:10:02 -0800

    adding election audit code

--- Pypoll.py
+++ Pypoll.py
@@ -23,6 +23,11 @@ candidate_options= []
 #Declare empty dictionary
 candidate_votes = {}
 
+#winning candidate and winning candidate tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 #Open the election results and read the file
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -46,9 +51,42 @@ with open(file_to_load) as election_data:
             #begin tracking that candidates vote count
             candidate_votes[candidate_name] = 0
 
+        #add a vote to that candidates count
+        candidate_votes[candidate_name]+=1
 
-#print candidate list
-print(candidate_votes)
+#determine the percentage of votes for each candidate by looping through the counts
+#1. Iterate through the candidate list
+for candidate_name in candidate_votes:
+    #2. Retrieve vote count of a candidate
+    votes= candidate_votes[candidate_name]
+    #3. Calculate the percentage of votes
+    vote_percentage = round(float(votes)/float(total_votes) *100,1)
+    
+    #Print out each candidates name, vote count, and percentage of votes to the terminal
+    print(f'{candidate_name} : {vote_percentage}% ({votes:,})\n')
+
+
+
+    #determing winning vote count and candidate
+    #1. determing if the votes are greater than the winning count
+    if (votes> winning_count) and (vote_percentage> winning_percentage):
+        #2. If true then set winning_count = votes and winning_percentage = vote_percentage
+        winning_count= votes
+        winning_percentage = vote_percentage
+        #3. Set the winning_candidate equal to the candidates name
+        winning_candidate = candidate_name
+
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f'Winner: {winning_candidate}\n'
+    f'Winning Vote Count: {winning_count:,}\n'
+    f'Winning Percentage: {winning_percentage}%\n')
+
+print(winning_candidate_summary)
+
+
+
+
 
 
 
